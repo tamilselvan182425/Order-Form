9cdea1b1661dca4d8d6a9510aedb8bcec459a5ff
tamilselvan182425 <146980415+tamilselvan182425@users.noreply.github.com>
2024-04-11 11:09:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import './index.css'
+
+import OrderForm from './OrderForm';
+
+
+
+function App() {
+  return (
+
+    <OrderForm />
+  
+   
+  
+  );
+}
+
+export default App;
